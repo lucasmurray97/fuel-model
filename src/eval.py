92cf0a40fea4f4f0b38dd36135e72a130858f21b
lucasmurray97 <37586981+lucasmurray97@users.noreply.github.com>
2024-06-05 15:46:52 -0400

    Improved eval modulde

--- src/eval.py
+++ src/eval.py
@@ -20,37 +20,54 @@ import rioxarray
 import sys
 from tqdm import tqdm
 from networks.ModisTempNet import ModisTempNet
+from networks.ConvNet import ConvNet
 sys.path.append("../")
 from data.utils import MyDataset, Resize
-from torcheval.metrics import MulticlassAccuracy, MulticlassConfusionMatrix
-
+from torcheval.metrics import MulticlassAccuracy, MulticlassConfusionMatrix, MulticlassF1Score, MulticlassPrecision, MulticlassRecall
+import json
 
 transform = transforms.Compose([Resize()])
 dataset = MyDataset(root="../data/Ventanas", tform = transform)
 
-train_dataset, validation_dataset, test_dataset =torch.utils.data.random_split(dataset, [0.8, 0.1, 0.1])
+generator = torch.Generator().manual_seed(123)
+train_dataset, validation_dataset, test_dataset =torch.utils.data.random_split(dataset, [0.7, 0.15, 0.15], generator)
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8)
 validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=8)
+test_loader = torch.utils.data.DataLoader(train_dataset, batch_size=8)
 
-net = ModisTempNet()
-net.load_state_dict(torch.load("networks/weights/Modis-Net_100.pth"))
+net = ConvNet()
+net.load_state_dict(torch.load("networks/weights/Conv-Net_100.pth"))
 net.eval()
 print(sum(p.numel() for p in net.parameters() if p.requires_grad))
 
-total_wins = 0
-total = 0
+net.cpu()
+net.eval()
 metric = MulticlassAccuracy()
 metric2 = MulticlassConfusionMatrix(20)
 metric3 = MulticlassAccuracy(average=None, num_classes=20)
-for x, y in tqdm(train_loader):
+metric4 = MulticlassF1Score(average="macro", num_classes=20)
+metric5 = MulticlassPrecision(average="macro", num_classes=20)
+metric6 = MulticlassRecall(average="macro", num_classes=20)
+for x, y in tqdm(validation_loader):
     pred = net(x)
     probs = net.softmax(pred)
     winners = probs.argmax(dim=1)
-    target = y.argmax(dim=1)
+    target = y.squeeze(1)
     metric.update(winners, target)
     metric2.update(winners, target)
     metric3.update(winners, target)
-print(metric.compute())
+    metric4.update(winners, target)
+    metric5.update(winners, target)
+    metric6.update(winners, target)
+results = {}
+results["accuracy"] = metric.compute().item()
+results["accuracy_per_class"] = metric3.compute().tolist()
+results["f1"] = metric4.compute().item()
+results["precision"] = metric5.compute().item()
+results["recall"] = metric6.compute().item()
+with open(f'plots/results_val_{net.name}_{100}.json', 'w') as f:
+    json.dump(results, f)
+
 confusion_matrix = metric2.compute()
 fig, ax = plt.subplots()
 fig.set_figwidth(15)
@@ -60,6 +77,4 @@ ax.matshow(confusion_matrix.numpy())
 
 for (i, j), z in np.ndenumerate(confusion_matrix.numpy()):
     ax.text(j, i, '{:0.1f}'.format(z), ha='center', va='center')
-plt.show()
-plt.savefig("cfm.png")
-print(metric3.compute())
+plt.savefig(f"plots/cfm_val_{net.name}_{100}.png")
